95edd3ac5361833e75f273396f8c742db011b9e1
aya <aya454062@gmail.com>
2026-01-28 19:17:35 +0200

    Fix Bounded component TypeScript issues

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,25 +1,25 @@
+import React from "react";
 import clsx from "clsx";
 
-type BoundedProps = {
-  as?: React.ElementType;
+export type BoundedProps<T extends React.ElementType = "section"> = {
+  as?: T;
   className?: string;
   children: React.ReactNode;
-};
+} & React.ComponentPropsWithoutRef<T>;
 
-export const Bounded = ({
-  as: Comp = "section",
+export const Bounded = <T extends React.ElementType = "section">({
+  as,
   className,
   children,
   ...restProps
-}: BoundedProps) => {
-  return (
-    <Comp
-      className={clsx("px-4 first:pt-10 md:px-6", className)}
-      {...restProps}
-    >
-      <div className="mx-auto flex w-full max-w-7xl flex-col items-center">
-        {children}
-      </div>
-    </Comp>
+}: BoundedProps<T>) => {
+  const Comp = as || "section";
+
+  return React.createElement(
+    Comp,
+    { className: clsx("px-4 first:pt-10 md:px-6", className), ...restProps },
+    <div className="mx-auto flex w-full max-w-7xl flex-col items-center">
+      {children}
+    </div>
   );
 };
